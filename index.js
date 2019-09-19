19f479026d32c54074fa8a39b540f17e28913f4d
Jack Lu <jacklu@jacklu.me>
2019-09-20 00:43:20 +0800

    +toplevel

--- index.js
+++ index.js
@@ -1,74 +1,78 @@
 function qlsql(ql) {
-	var sql;
-	switch (ql.type) {
-		case 'literal':
-			sql = {
-				type: 'literal',
-				value: ql.value
-			};
-			break;
-		case 'name':
-			sql = {
-				type: 'name',
-				identifier: ql.identifier
-			};
-			break;
-		case 'property':
-			sql = {
-				type: 'select',
-				field: [{ type: 'name', identifier: ql.property }],
-				from: qlsql(ql.expression)
-			};
-			break;
-		case 'index':
-			sql = {
-				type: 'select',
-				field: [{ type: 'name', identifier: '*' }],
-				from: qlsql(ql.expression),
-				where: qlsql({
+	var sql = qlsql(ql);
+	return selectize(sql);
+	function qlsql(ql) {
+		var sql;
+		switch (ql.type) {
+			case 'literal':
+				sql = {
+					type: 'literal',
+					value: ql.value
+				};
+				break;
+			case 'name':
+				sql = {
+					type: 'name',
+					identifier: ql.identifier
+				};
+				break;
+			case 'property':
+				sql = {
+					type: 'select',
+					field: [{ type: 'name', identifier: ql.property }],
+					from: qlsql(ql.expression)
+				};
+				break;
+			case 'index':
+				sql = {
+					type: 'select',
+					field: [{ type: 'name', identifier: '*' }],
+					from: qlsql(ql.expression),
+					where: qlsql({
+						type: 'binary',
+						operator: '=',
+						left: { type: 'name', identifier: 'id' },
+						right: ql.index
+					})
+				};
+				break;
+			case 'unary':
+				sql = {
+					type: 'unary',
+					operator: ql.operator,
+					operand: qlsql(ql.operand)
+				};
+				break;
+			case 'binary':
+				sql = {
 					type: 'binary',
-					operator: '=',
-					left: { type: 'name', identifier: 'id' },
-					right: ql.index
-				})
-			};
-			break;
-		case 'unary':
-			sql = {
-				type: 'unary',
-				operator: ql.operator,
-				operand: qlsql(ql.operand)
-			};
-			break;
-		case 'binary':
-			sql = {
-				type: 'binary',
-				operator: ql.operator,
-				left: qlsql(ql.left),
-				right: qlsql(ql.right)
-			};
-			break;
-		case 'filter':
-			sql = {
-				type: 'select',
-				field: [{ type: 'name', identifier: '*' }],
-				from: qlsql(ql.expression),
-				where: qlsql(ql.filter)
-			};
-			break;
-		case 'comma':
-			sql = {
-				type: 'select',
-				with: {
-					name: ql.head.name,
-					value: selectize(qlsql(ql.head.value))
-				},
-				field: [{ type: 'name', identifier: '*' }],
-				from: qlsql(ql.body)
-			};
-			break;
+					operator: ql.operator,
+					left: qlsql(ql.left),
+					right: qlsql(ql.right)
+				};
+				break;
+			case 'filter':
+				sql = {
+					type: 'select',
+					field: [{ type: 'name', identifier: '*' }],
+					from: qlsql(ql.expression),
+					where: qlsql(ql.filter)
+				};
+				break;
+			case 'comma':
+				sql = {
+					type: 'select',
+					with: {
+						name: ql.head.name,
+						value: selectize(qlsql(ql.head.value))
+					},
+					field: [{ type: 'name', identifier: '*' }],
+					from: qlsql(ql.body)
+				};
+				break;
+		}
+		return sql;
 	}
-	return sql;
 }
 
 function selectize(sql) {
@@ -87,7 +91,4 @@ function selectize(sql) {
 	return sql;
 }
 
-module.exports = function (ql) {
-	var sql = qlsql(ql);
-	return selectize(sql);
-};
+module.exports = qlsql;
